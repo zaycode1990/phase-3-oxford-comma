af38de3bfcaa4f448b7b16bf42df992ef5ff0c04
Isaiah <ijandicos90@gmail.com>
2022-07-14 22:59:51 -0400

    Completed Assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
+require "pry"
 
+def oxford_comma(array)
+last_item = array.last
+if array.length < 3 
+    array.join(" and ")
+else
+array[-1] = "and #{last_item}"
+array.join(", ")
+ end
 end
+
